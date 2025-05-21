9a59d852a0e3cf52c63e3f3fe96baf60dd763377
NaveenKari <karinaveen0987@gmail.com>
2025-05-21 15:21:29 -0500

    Time: 2 ms (74.98%) | Memory: 53.3 MB (39.20%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,24 +1,47 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        //Optimized approach
+        // element will repeat more than half of the array.
         int res = nums[0];
-        int count = 0;
-        int target = (nums.length)/2;
-        for(int i = 0; i < nums.length; i++){
-            if(res != nums[i]){
+        int count = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            if(count==0){
                 res = nums[i];
+                count++;
             }
-            for ( int j : nums){
-               // System.out.println(res);
-                if(j == res){
-                    count+=1;
-                }
+            else if(nums[i]==res){
+                count++;
             }
-           // System.out.println(count);
-            if(count > target){
-                break;
+            else{
+                count--;
             }
-            count = 0;
         }
+
         return res;
+
+
+
+
+        // int res = nums[0];
+        // int count = 0;
+        // int target = (nums.length)/2;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(res != nums[i]){
+        //         res = nums[i];
+        //     }
+        //     for ( int j : nums){
+        //        // System.out.println(res);
+        //         if(j == res){
+        //             count+=1;
+        //         }
+        //     }
+        //    // System.out.println(count);
+        //     if(count > target){
+        //         break;
+        //     }
+        //     count = 0;
+        // }
+        // return res;
     }
 }
