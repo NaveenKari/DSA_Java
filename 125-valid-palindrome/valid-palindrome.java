1b608f9519644babac72533ea5c6d526f3ecf52e
NaveenKari <karinaveen0987@gmail.com>
2026-02-03 10:11:03 -0600

    Time: 2 ms (98.84%) | Memory: 44.5 MB (65.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,25 +1,44 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-            return true;
-        }
-        int start = 0;
-        int last = s.length() - 1;
-        while (start <= last) {
-            char currFirst = s.charAt(start);
-            char currLast = s.charAt(last);
-            if (!Character.isLetterOrDigit(currFirst)) {
-                start++;
-            } else if (!Character.isLetterOrDigit(currLast)) {
-                last--;
-            } else {
-                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-                    return false;
-                }
-                start++;
-                last--;
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+
+            if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))){
+                i = i+1;
+                j = j-1;
+                continue;
             }
+            return false;
         }
         return true;
+
+
+
+        // StringBuilder test = new StringBuilder();
+        // for(int i = 0; i<s.length();i++){
+        //     if(Character.isLetterOrDigit(s.charAt(i))){
+        //         test.append(Character.toLowerCase(s.charAt(i)));
+        //     }
+
+        //     continue;
+
+        // }
+        // int i =0;
+        // int j = test.length()-1;
+        // System.out.println(test);
+        // while(i<j){
+        //     if(test.charAt(i)==test.charAt(j)){
+        //         i= i+1;
+        //         j= j-1;
+        //         continue;
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+
     }
 }
