d8cd95ecdd013290d803515509a0f8310ea7a167
NaveenKari <karinaveen0987@gmail.com>
2026-02-04 11:15:47 -0600

    Time: 3 ms (19.61%) | Memory: 47.8 MB (71.82%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,24 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //My approach
-        int i = 0;
-        int j = 1;
-        while(j<nums.length){
-            if(nums[i] == 0 && nums[j]!=0){
-               // System.out.println("entered");
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i+=1;
-                j+=1;
-            }
-            else if(nums[i] == 0 && nums[j]==0){
-                j+=1;
-            }
-            else{
-                i+=1;
-                j+=1;
-            }
-        }  
+
+    int i = 0;
+
+    for(int j = 0; j<nums.length;j++){
+        if(nums[j]!=0){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j]= temp;
+            i++;
+        }
+    }
+
+    //     if(nums.length == 1 || nums.length == 0) return;
+
+    //     int i = 0;
+    //     int j = 1;
+
+    //     while(j<nums.length){
+    //         if(nums[i]== 0 && nums[j]!=0){
+    //             int temp = nums[i];
+    //             nums[i] = nums[j];
+    //             nums[j] = temp;
+    //             i = i+1;
+    //             j= j+1;
+
+    //         }else if(nums[i]!=0 && nums[j]!=0){
+    //             i = i+1;
+    //             j= j+1;
+    //         }
+    //         else if(nums[i]==0 && nums[j]==0){
+    //             j = j+1;
+    //         }else{
+    //             i = i+1;
+    //             j = j+1;
+    //         }
+    //     }
     }
 }
