c2d6e542a784207d99af23ece99f7b088141ae9a
NaveenKari <karinaveen0987@gmail.com>
2026-02-06 21:33:23 -0600

    Time: 5 ms (85.73%) | Memory: 45.2 MB (85.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,27 +1,47 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int slen = s.length();
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        int left = 0;
+        int best = 0;
 
-        if(slen == 0 || slen == 1) return slen;
-        int res = 0;
-        Map<Character,Integer> hmap = new HashMap<>();
-        
-        for(int i = 0;i<s.length();i++){
-            int j = i;
-            while(j<slen){
-                if(hmap.containsKey(s.charAt(j))){
-                    res = Math.max(res,hmap.size());
-                    //System.out.println(hmap);
-                    hmap.clear();
-                    break;
-                    
-                }else{
-                    hmap.put(s.charAt(j),j);
-                    j++;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (lastIndex.containsKey(c)) {
+                left = Math.max(left, lastIndex.get(c) + 1);
             }
-            }   
+
+            lastIndex.put(c, right);
+            best = Math.max(best, right - left + 1);
         }
-        return res;
+
+        return best;
+
+
+
+
+        // int slen = s.length();
+
+        // if(slen == 0 || slen == 1) return slen;
+        // int res = 0;
+        // Map<Character,Integer> hmap = new HashMap<>();
+        
+        // for(int i = 0;i<s.length();i++){
+        //     int j = i;
+        //     while(j<slen){
+        //         if(hmap.containsKey(s.charAt(j))){
+        //             res = Math.max(res,hmap.size());
+        //             //System.out.println(hmap);
+        //             hmap.clear();
+        //             break;
+                    
+        //         }else{
+        //             hmap.put(s.charAt(j),j);
+        //             j++;
+        //     }
+        //     }   
+        // }
+        // return res;
 
         }
     
