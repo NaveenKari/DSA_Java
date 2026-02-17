98332bb910ce39b758629fb4bbceefea58809d6c
NaveenKari <karinaveen0987@gmail.com>
2026-02-16 22:17:09 -0600

    Time: 4 ms (57.18%) | Memory: 47.1 MB (15.40%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,58 +1,28 @@
 class Solution {
     public int romanToInt(String s) {
-        int res = 0,track = 0;
-        Map<Character,Integer> table = new HashMap<>();
-        table.put('I',1);
-        table.put('V',5);
-        table.put('X',10);
-        table.put('L',50);
-        table.put('C',100);
-        table.put('D',500);
-        table.put('M',1000);
+         Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
-        if(s.length()==1) return table.get(s.charAt(0));
+        int res = 0;
 
-        for(int i = 0; i<s.length()-1;i++){
-            char t = s.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
+            int curr = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
 
-            if(t == 'I'){
-                if(s.charAt(i+1)=='V'){
-                    i = i+1;
-                    res+= 4;
-                }else if(s.charAt(i+1)=='X'){
-                    i = i+1;
-                    res+= 9;
-                }else{
-                res+=table.get(t);
+            if (curr < next) {
+                res -= curr;
+            } else {
+                res += curr;
             }
-            }else if(t == 'X'){
-                if(s.charAt(i+1)=='L'){
-                    i = i+1;
-                    res+= 40;
-                }else if(s.charAt(i+1)=='C'){
-                    i = i+1;
-                    res+= 90;
-                }else{
-                res+=table.get(t);
-            }
-            }else if(t == 'C'){
-                if(s.charAt(i+1)=='D'){
-                    i = i+1;
-                    res+= 400;
-                }else if(s.charAt(i+1)=='M'){
-                    i = i+1;
-                    res+= 900;
-                }else{
-                res+=table.get(t);
-            }
-            }else{
-                res+=table.get(t);
-            }
-            track = i;
-            
         }
-        if(track == s.length()-1) return res;
-        res+=table.get(s.charAt(s.length()-1));
+
         return res;
     }
+    
 }
