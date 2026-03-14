f986ebbf1ebf0cc9538ebb0f81ea6a37bbe54794
NaveenKari <karinaveen0987@gmail.com>
2026-03-13 19:54:35 -0500

    Time: 1 ms (96.74%) | Memory: 46.1 MB (74.64%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,21 +14,32 @@
  * }
  */
 class Solution {
+    private Map<Integer,Integer> hmap = new HashMap<>();
+    private int preIndex = 0;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length == 0 || inorder.length == 0) return null;
-
-        TreeNode root = new TreeNode(preorder[0]);
-        int index = 0;
-
-        for(int i = 0; i<inorder.length ; i++){
-            if(inorder[i] == preorder[0]) {
-                index = i;
-            }
+        for(int i = 0; i< inorder.length;i++){
+            hmap.put(inorder[i],i);
         }
 
-        root.left = buildTree(Arrays.copyOfRange(preorder,1,index+1),Arrays.copyOfRange(inorder,0,index+1));
-        root.right = buildTree(Arrays.copyOfRange(preorder,index+1,preorder.length),Arrays.copyOfRange(inorder,index+1,inorder.length));
+        return build(preorder,0,inorder.length-1);
+    }
+
+    private TreeNode build(int[] preorder, int left, int right){
+        if(left > right) {
+         return null;
+        }
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int mid = hmap.get(rootVal);
+
+        root.left = build(preorder,left,mid-1);
+        root.right = build(preorder,mid+1, right);
 
         return root;
+
     }
+
+ 
 }
